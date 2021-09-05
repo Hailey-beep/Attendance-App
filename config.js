61d442bc8cb6cf2a9a79cc4c0103374d4fbfb17e
Hailey <72172315+Hailey-beep@users.noreply.github.com>
2021-09-05 19:50:44 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,9 @@
  import firebase from "firebase";
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBr4V6hfQNI0HLjPS5UnKNpLJN_EcqUbKI",
-    authDomain: "attendance-app-5f0bd.firebaseapp.com",
-    databaseURL: "https://attendance-app-5f0bd-default-rtdb.firebaseio.com",
-    projectId: "attendance-app-5f0bd",
-    storageBucket: "attendance-app-5f0bd.appspot.com",
-    messagingSenderId: "148534215253",
-    appId: "1:148534215253:web:6a31fbdf867e066857cadc"
+  
+  //Insert database Config
+ 
   };
   // Initialize Firebase
   if(!firebase.apps.length) {
